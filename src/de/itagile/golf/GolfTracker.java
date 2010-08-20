91ccd18ebb73306d03461ed5f07962b20d0fcb41
Bernd Schiffer <bernd.schiffer@gmail.com>
2010-08-20 14:50:57 +0200

    Akzeptanztests für Story "ball_schlagen" geschrieben. -- bs

--- src/de/itagile/golf/GolfTracker.java
+++ src/de/itagile/golf/GolfTracker.java
@@ -3,7 +3,6 @@ package de.itagile.golf;
 public class GolfTracker {
 
     public String reagiereAuf(String befehl) {
-        return "Du hast einen Schlag.";
+        return "Du hast 1 Schlag.";
     }
-
 }
