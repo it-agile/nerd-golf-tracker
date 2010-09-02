9a6bafcdf5d3ced24e19b39cf061b7a779fdb3ee
Alex Bepple <alex@bepple.de>
2010-09-02 17:19:28 +0200

    Startoperation für Tracker definiert.

--- src/de/itagile/golf/Main.java
+++ src/de/itagile/golf/Main.java
@@ -4,14 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import de.itagile.golf.operation.Schlagzahlausgabe;
+
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		Tracker tracker = new Tracker(new EinfacherInterpreter());
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		tracker.setStartoperation(new Schlagzahlausgabe());
 		
 		System.out.println("auf dem 1. Loch");
 		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			String befehl = reader.readLine();
 			System.out.println(tracker.reagiereAuf(befehl));
