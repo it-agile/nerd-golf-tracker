6fed28522367926646b09aa2c2ac46210923374f
Bernd Schiffer <bernd.schiffer@gmail.com>
2010-09-02 16:02:08 +0200

    Der Work-in-Progress von Alex und Sven wird jetzt @Ignore-iert. --ms&bs

--- specification/de/itagile/golf/AktuellesLochAnzeigen.java
+++ specification/de/itagile/golf/AktuellesLochAnzeigen.java
@@ -1,7 +1,10 @@
 package de.itagile.golf;
 
+import org.junit.Ignore;
+
 import de.itagile.golf.jbehave.BaseScenario;
 
+@Ignore("wip von Alex und Sven")
 public class AktuellesLochAnzeigen extends BaseScenario {
 
 	public AktuellesLochAnzeigen() {
