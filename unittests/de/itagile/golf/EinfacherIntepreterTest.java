5aa7b09816e4d73b50e5dc6219bff2b241644643
Alex Bepple <alex@bepple.de>
2010-09-02 15:36:37 +0200

    Tests in parametrisierten Test überführt.

--- unittests/de/itagile/golf/EinfacherIntepreterTest.java
+++ unittests/de/itagile/golf/EinfacherIntepreterTest.java
@@ -3,22 +3,40 @@ package de.itagile.golf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import de.itagile.golf.operation.Lochwechsel;
 import de.itagile.golf.operation.Schlag;
 
+@RunWith(Parameterized.class)
 public class EinfacherIntepreterTest {
 
 	private EingabeInterpreter interpreter = new EinfacherInterpreter();
+	private final String befehl;
+	private final Class<? super Operation> operation;
 
-	@Test
-	public void interpretiertBallschlag() throws Exception {
-		assertThat(interpreter.interpretiere("Schlage Ball"), is(Schlag.class));
+	public EinfacherIntepreterTest(String befehl, Class<? super Operation> operation) {
+		this.befehl = befehl;
+		this.operation = operation;
+	}
+	
+	@Parameters
+	public static List<Object[]> parameters() {
+		Object[][] testData = new Object[][] {
+				{"Schlage Ball", Schlag.class},
+				{"Nächstes Loch", Lochwechsel.class},
+				};
+		return Arrays.asList(testData);
 	}
 	
 	@Test
-	public void interpretiertLochwechsel() throws Exception {
-		assertThat(interpreter.interpretiere("Nächstes Loch"), is(Lochwechsel.class));
+	public void interpretiertBefehle() throws Exception {
+		assertThat(interpreter.interpretiere(befehl), is(operation));
 	}
 }
