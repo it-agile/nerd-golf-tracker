a6db26793239b150a347c78947221a9addc78061
Marko Schulz <marko.schulz@it-agile.de>
2010-09-17 13:34:21 +0200

    Tests für die Lochwechsel-Operation nachgezogen. -- ah&ms

--- unittests/de/itagile/golf/operation/LochwechselTest.java
+++ unittests/de/itagile/golf/operation/LochwechselTest.java
@@ -1,8 +1,12 @@
 package de.itagile.golf.operation;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.itagile.golf.Scorecard;
@@ -10,10 +14,29 @@ import de.itagile.golf.Scorecard;
 
 public class LochwechselTest {
 	
+	private Scorecard scorecard;
+	private Lochwechsel lochwechsel;
+
+	@Before
+	public void setup() {
+		scorecard = mock(Scorecard.class);
+		lochwechsel = new Lochwechsel();		
+	}
+	
 	@Test
 	public void setztAnzahlSchlaegeZurueck() throws Exception {
-		Scorecard scorecard = mock(Scorecard.class);
-		new Lochwechsel().fuehreAus(scorecard);
+		lochwechsel.fuehreAus(scorecard);
 		verify(scorecard).setzeZurueckAnzahlSchlaege();
+	}
+	
+	@Test
+	public void gibtZuruecksetzenAus() throws Exception {
+		assertThat(lochwechsel.fuehreAus(scorecard), containsString("Deine Schlagzahl wurde zurück gesetzt"));
+	}
+	
+	@Test
+	public void gibtNeuesLochAus() throws Exception {
+		when(scorecard.aktuellesLoch()).thenReturn(2);
+		assertThat(lochwechsel.fuehreAus(scorecard), containsString("Du bist auf dem 2. Loch"));		
 	}
 }
