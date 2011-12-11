e6d770f75b7cc0c94d84b5fd7f57febefdc27413
Alex Bepple <alex@bepple.de>
2011-12-12 00:53:14 +0100

    Benutze UTF-8 überall
    So kann ein Kompilat von einer Plattform überall eingesetzt werden. Auf
    der Windows-XP-Konsole hat man aber immer noch ein Problem.

--- src/de/itagile/golf/Main.java
+++ src/de/itagile/golf/Main.java
@@ -3,18 +3,27 @@ package de.itagile.golf;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 import de.itagile.golf.operation.Lochausgabe;
 import de.itagile.golf.operation.Startausgabe;
 
 public class Main {
 	
+	private static final String CHARSET = "UTF-8";
+	private static PrintWriter writer;
+	private static BufferedReader reader;
+
 	public static void main(String[] args) throws IOException {
+		writer = new PrintWriter(new OutputStreamWriter(System.out, CHARSET));
+		reader = new BufferedReader(new InputStreamReader(System.in, CHARSET));
+		
 		Startausgabe startoperation = new Startausgabe(new Lochausgabe());
 		Tracker tracker = new Tracker(new EinfacherInterpreter(), startoperation);
+
 		gibAus(tracker.starte());
 		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			String befehl = reader.readLine();
 			gibAus(tracker.reagiereAuf(befehl));
@@ -22,6 +31,7 @@ public class Main {
 	}
 
 	private static void gibAus(String ausgabe) {
-		System.out.println(ausgabe);
+		writer.println(ausgabe);
+		writer.flush();
 	}
 }
