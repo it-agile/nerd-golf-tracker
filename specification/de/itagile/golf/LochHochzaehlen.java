8ff8de71499fa482984508696c9a37cff74c9eda
Alex Bepple <alex@bepple.de>
2012-09-06 23:14:17 +0200

    o Intuitiverer Name für Akzeptanztest

--- specification/de/itagile/golf/LochHochzaehlen.java
+++ specification/de/itagile/golf/LochHochzaehlen.java
@@ -2,9 +2,9 @@ package de.itagile.golf;
 
 import de.itagile.golf.jbehave.BaseScenario;
 
-public class AktuellesLochAnzeigen extends BaseScenario {
+public class LochHochzaehlen extends BaseScenario {
 
-	public AktuellesLochAnzeigen() {
+	public LochHochzaehlen() {
 		TrackerDriver driver = new TrackerDriver();
 		addSteps(new AktuellesLochAnzeigenSteps(driver),
 				 new BallSchlagenSteps(driver),
