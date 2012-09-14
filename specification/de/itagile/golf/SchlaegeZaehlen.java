b70f63e006585bf9a8bd56961b7505ff029446b6
Alex Bepple <alex@bepple.de>
2012-09-14 20:39:35 +0200

    o "Ball schlagen" -> "Schläge zählen"

--- specification/de/itagile/golf/SchlaegeZaehlen.java
+++ specification/de/itagile/golf/SchlaegeZaehlen.java
@@ -3,9 +3,9 @@ package de.itagile.golf;
 import de.itagile.golf.jbehave.BaseScenario;
 
 
-public class BallSchlagen extends BaseScenario {
+public class SchlaegeZaehlen extends BaseScenario {
 	
-	public BallSchlagen() {
+	public SchlaegeZaehlen() {
 		TrackerDriver driver = new TrackerDriver();
 		addSteps(new BallSchlagenSteps(driver),
 				 new SchlaegeZaehlenSteps(driver));
