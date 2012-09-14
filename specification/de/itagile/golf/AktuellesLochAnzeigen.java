0f9f00ed5847ec6b9bbeb64dc1b8ab7e5d0046dd
Alex Bepple <alex@bepple.de>
2012-09-14 19:51:33 +0200

    o "Loch anzeigen" statt "Loch hochzählen"
    Der neue Name hat bei Kurs-Teilnehmern Verwirrung verursacht.

--- specification/de/itagile/golf/AktuellesLochAnzeigen.java
+++ specification/de/itagile/golf/AktuellesLochAnzeigen.java
@@ -2,9 +2,9 @@ package de.itagile.golf;
 
 import de.itagile.golf.jbehave.BaseScenario;
 
-public class LochHochzaehlen extends BaseScenario {
+public class AktuellesLochAnzeigen extends BaseScenario {
 
-	public LochHochzaehlen() {
+	public AktuellesLochAnzeigen() {
 		TrackerDriver driver = new TrackerDriver();
 		addSteps(new AktuellesLochSteps(driver),
 				new VerschiedeneSteps(driver),
