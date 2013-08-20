120e73cd32bbcb499f274e80cc7d439da828c60b
Alex Bepple <alex@bepple.de>
2013-08-20 16:57:37 +0200

    ! Pending Steps failen den Build

--- cucumber/java/de/itagile/golf/HelloSteps.java
+++ cucumber/java/de/itagile/golf/HelloSteps.java
@@ -7,6 +7,6 @@ import cucumber.api.java.en.Then;
 public class HelloSteps {
     @Then("buh")
     public void it_should_answer_with() {
-        assertThat(1+1, is(3));
+        assertThat(1+1, is(2));
     }
 }
