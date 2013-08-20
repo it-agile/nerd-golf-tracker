44c001b83116dd2290821f20245c14163a8c53e5
Alex Bepple <alex@bepple.de>
2013-08-20 22:07:54 +0200

    + Feature "Schläge zurücksetzen" @in_arbeit

--- cucumber/java/de/itagile/golf/SchlaegeStepdefs.java
+++ cucumber/java/de/itagile/golf/SchlaegeStepdefs.java
@@ -1,5 +1,6 @@
 package de.itagile.golf;
 
+import static java.lang.String.valueOf;
 import static org.hamcrest.Matchers.containsString;
 import cucumber.api.java.de.Dann;
 import cucumber.api.java.de.Wenn;
@@ -21,8 +22,21 @@ public class SchlaegeStepdefs {
         }
     }
 	
+	@Wenn("ich auf einem Loch gespielt habe")
+    public void spieleAufEinemLochUndGeheWeiter() {
+        tracker.gibEin("Schlage Ball");
+        tracker.gibEin("Nächstes Loch");
+    }
+	
 	@Dann("zählt der NerdGolfTracker auch so viele Schläge")
     public void pruefeSchlaege() {
-        tracker.assertThatAntwort(containsString(String.valueOf(schlaege)));
+        tracker.assertThatAntwort(containsString(valueOf(schlaege)));
     }
+	
+	@Dann("zählen die Schläge auf dem nächsten Loch wieder von 0 an")
+	public void pruefeSchlaegeZaehlen() {
+        tracker.gibEin("Schlage Ball");
+		tracker.assertThatAntwort(containsString(valueOf(1)));
+	}
+
 }
---
+++ cucumber/resources/de/itagile/golf/Lochwechsel.feature
@@ -0,0 +1,9 @@
+# language: de
+
+@in_arbeit
+Funktionalität: Lochwechsel
+
+    Szenario: Normalfall
+        Wenn ich auf einem Loch gespielt habe
+        Dann zählen die Schläge auf dem nächsten Loch wieder von 0 an
+
