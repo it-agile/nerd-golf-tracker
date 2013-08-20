497029de46b345be3ec7d904345635feeddbc44c
Alex Bepple <alex@bepple.de>
2013-08-20 20:57:28 +0200

    + Stepdefs für "Aktuelles Loch anzeigen" implementiert

--- cucumber/java/de/itagile/golf/AktuellesLochStepdefs.java
+++ cucumber/java/de/itagile/golf/AktuellesLochStepdefs.java
@@ -1,11 +1,25 @@
 package de.itagile.golf;
 
+import static org.hamcrest.Matchers.containsString;
 import cucumber.api.java.de.Dann;
 
 public class AktuellesLochStepdefs {
 
+	private TrackerDriver tracker;
+
+	public AktuellesLochStepdefs(TrackerDriver tracker) {
+		this.tracker = tracker;
+	}
+	
 	@Dann("bin ich auf dem (\\d+). Loch")
 	public void pruefeAktuellesLoch(int erwartetesLoch) {
+		pruefeLochAnzeige(erwartetesLoch);
+		tracker.gibEin("Schlage Ball");
+		pruefeLochAnzeige(erwartetesLoch);
+	}
+
+	private void pruefeLochAnzeige(int loch) {
+		tracker.assertThatAntwort(containsString(String.format("%d.Loch", loch)));
 	}
 	
 }
--- cucumber/java/de/itagile/golf/NerdGolfTrackerStepdefs.java
+++ cucumber/java/de/itagile/golf/NerdGolfTrackerStepdefs.java
@@ -1,8 +1,26 @@
 package de.itagile.golf;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.de.Wenn;
 
 public class NerdGolfTrackerStepdefs {
+	
+	private TrackerDriver tracker;
+
+	public NerdGolfTrackerStepdefs(TrackerDriver tracker) {
+		this.tracker = tracker;
+	}
+
+	@Before
+    public void starteNeuesSpiel() {
+        tracker.starte();
+    }
+
+	@After
+    public void beendeTracker() {
+        tracker.beende();
+    }
 
 	@Wenn("ich den NerdGolfTracker starte")
     public void starteNerdGolfTracker() {
