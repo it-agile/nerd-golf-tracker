87e9e20ad991dfd44fc2c8d7bc62d8feceba2da0
Alex Bepple <alex@bepple.de>
2013-08-20 21:50:59 +0200

    + Hilfe-Feature

---
+++ cucumber/java/de/itagile/golf/HilfeStepdefs.java
@@ -0,0 +1,24 @@
+package de.itagile.golf;
+
+import static org.hamcrest.Matchers.containsString;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HilfeStepdefs {
+	
+	private TrackerDriver tracker;
+
+	public HilfeStepdefs(TrackerDriver tracker) {
+		this.tracker = tracker;
+	}
+	
+	@When("ich die Hilfe aufrufe")
+	public void rufeHilfeAuf() {
+		tracker.gibEin("Hilfe");
+	}
+	
+	@Then("listet der NerdGolfTracker einen Eintrag zu (.*) auf")
+	public void pruefeHilfeEintrag(String kommando) {
+		tracker.assertThatAntwort(containsString(kommando));
+	}
+}
---
+++ cucumber/resources/de/itagile/golf/Hilfe.feature
@@ -0,0 +1,13 @@
+# language: de
+
+Funktionalität: Hilfe
+
+    Szenariogrundriss: Eintrag in der Hilfe
+        Wenn ich die Hilfe aufrufe
+        Dann listet der NerdGolfTracker einen Eintrag zu <Kommando> auf
+
+        Beispiele:
+            |Kommando|
+            |Schlage Ball|
+            |Nächstes Loch|
+            |Hilfe|
