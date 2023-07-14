ae5200fc84e7c00c41ecb53ae9ffb087cfa1bb83
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-07-14 18:32:30 +0200

    purged CollectionUtils

--- src/main/java/de/itagile/golf/operation/Hilfe.java
+++ src/main/java/de/itagile/golf/operation/Hilfe.java
@@ -1,13 +1,11 @@
 package de.itagile.golf.operation;
 
-import java.util.ArrayList;
-import java.util.List;
+import static java.util.stream.Collectors.joining;
 
 import de.itagile.golf.Befehl;
 import de.itagile.golf.BefehleSammler;
 import de.itagile.golf.Operation;
 import de.itagile.golf.Scorecard;
-import de.itagile.golf.util.CollectionUtils;
 import de.itagile.golf.util.SystemProperties;
 
 public class Hilfe implements Operation {
@@ -20,11 +18,8 @@ public class Hilfe implements Operation {
 
 	@Override
 	public String fuehreAus(Scorecard scorecard) {
-		List<String> hilfeTexte = new ArrayList<>();
-		for (Befehl befehl : sammler.sammle()) {
-			hilfeTexte.add(hilfeZeileFuer(befehl));
-		}
-		return "Ich reagiere auf: " + CollectionUtils.join(hilfeTexte, SystemProperties.LINE_SEPARATOR);
+		return "Ich reagiere auf: "
+				+ sammler.sammle().stream().map(this::hilfeZeileFuer).collect(joining(SystemProperties.LINE_SEPARATOR));
 	}
 
 	private String hilfeZeileFuer(Befehl befehl) {
